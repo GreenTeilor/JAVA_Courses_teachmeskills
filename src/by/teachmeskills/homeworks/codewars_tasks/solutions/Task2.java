2647311e8c34d9ef5496d080a5689612910e14d2
Baranouski Raman <baranouski.raman@gmail.com>
2023-02-04 15:52:59 +0300

    Solve 2-nd problem

---
+++ src/by/teachmeskills/homeworks/codewars_tasks/solutions/Task2.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.homeworks.codewars_tasks.solutions;
+
+
+public class Task2 {
+    public static int longestSlideDown(int[][] pyramid) {
+        for (int i = pyramid.length - 2; i >= 0; --i) {
+            for (int j = 0; j < pyramid[i].length; ++j) {
+                pyramid[i][j] += Math.max(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
+            }
+        }
+        return pyramid[0][0];
+    }
+}
