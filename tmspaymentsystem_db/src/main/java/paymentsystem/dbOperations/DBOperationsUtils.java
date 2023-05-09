53704c87a3b046fb4f3db51060a8892285dc7c9b
Baranouski Raman <baranouski.raman@gmail.com>
2023-05-10 00:20:16 +0300

    Moved all queries to static final fields

--- tmspaymentsystem_db/src/main/java/paymentsystem/dbOperations/DBOperationsUtils.java
+++ tmspaymentsystem_db/src/main/java/paymentsystem/dbOperations/DBOperationsUtils.java
@@ -16,13 +16,23 @@ import java.util.UUID;
 import java.util.function.Function;
 
 public class DBOperationsUtils {
+    private final static String ADD_QUERY = "INSERT INTO merchants (id, name, created_at) Values (?, ?, ?)";
+    private final static String SEARCH_ACCOUNT_QUERY = "SELECT * FROM bank_accounts WHERE merchant_id = ? AND account_number = ?";
+    private final static String UPDATE_STATUS_QUERY = "UPDATE bank_accounts SET status = ? WHERE merchant_id = ? AND account_number = ?";
+    private final static String CREATE_QUERY = "INSERT INTO bank_accounts (id, merchant_id, status, account_number, created_at) Values (?, ?, ?, ?, ?)";
+    private final static String GET_MERCHANTS_QUERY = "SELECT * FROM merchants";
+    private final static String GET_ACCOUNTS_QUERY = "SELECT * FROM bank_accounts ORDER BY status ASC, created_at ASC";
+
+    private final static String UPDATE_ACCOUNT_NUMBER_QUERY = "UPDATE bank_accounts SET account_number = ? WHERE merchant_id = ? AND account_number = ?";
+    private final static String DELETE_ACCOUNTS_QUERY = "DELETE FROM bank_accounts WHERE merchant_id = ?";
+    private final static String DELETE_MERCHANT_QUERY = "DELETE FROM merchants WHERE id = ?";
+
     private DBOperationsUtils() {
 
     }
 
     public static void saveMerchantDB(Connection connection, Merchant merchant) {
-        String query = "INSERT INTO merchants (id, name, created_at) Values (?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(query)){
+        try (PreparedStatement statement = connection.prepareStatement(ADD_QUERY)){
             statement.setString(1, merchant.getId());
             statement.setString(2, merchant.getName());
             statement.setTimestamp(3, Timestamp.valueOf(merchant.getCreationDate().atStartOfDay()));
@@ -33,9 +43,6 @@ public class DBOperationsUtils {
     }
 
     public static void saveBankAccountDB(Connection connection, BankAccount bankAccount) {
-        String querySEARCH = "SELECT * FROM bank_accounts WHERE merchant_id = ? AND account_number = ?";
-        String queryUPDATE = "UPDATE bank_accounts SET status = ? WHERE merchant_id = ? AND account_number = ?";
-        String queryCREATE = "INSERT INTO bank_accounts (id, merchant_id, status, account_number, created_at) Values (?, ?, ?, ?, ?)";
         Function<String, Boolean> isPresent = query -> {
             try(PreparedStatement statement = connection.prepareStatement(query)) {
                 statement.setString(1, bankAccount.getMerchantId());
@@ -51,8 +58,8 @@ public class DBOperationsUtils {
                 throw new RuntimeException();
             }
         };
-        if (isPresent.apply(querySEARCH)) {
-            try(PreparedStatement statement = connection.prepareStatement(queryUPDATE)) {
+        if (isPresent.apply(SEARCH_ACCOUNT_QUERY)) {
+            try(PreparedStatement statement = connection.prepareStatement(UPDATE_STATUS_QUERY)) {
                 statement.setString(1, "active");
                 statement.setString(2, bankAccount.getMerchantId());
                 statement.setString(3, EncryptorDecryptorUtils.encrypt(bankAccount.getAccountNumber()));
@@ -61,7 +68,7 @@ public class DBOperationsUtils {
                 System.out.println(e.getMessage());
             }
         } else {
-            try(PreparedStatement statement = connection.prepareStatement(queryCREATE)) {
+            try(PreparedStatement statement = connection.prepareStatement(CREATE_QUERY)) {
                 statement.setString(1, String.valueOf(UUID.randomUUID()));
                 statement.setString(2, bankAccount.getMerchantId());
                 statement.setString(3, bankAccount.getStatus().toString());
@@ -77,9 +84,8 @@ public class DBOperationsUtils {
 
     public static List<Merchant> readMerchantsDB(Connection connection) {
         List<Merchant> merchants = new ArrayList<>();
-        String query = "SELECT * FROM merchants";
         try (Statement statement = connection.createStatement()) {
-            ResultSet set = statement.executeQuery(query);
+            ResultSet set = statement.executeQuery(GET_MERCHANTS_QUERY);
             while (set.next()) {
                 merchants.add(new Merchant(set.getString(1), set.getString(2), set.getTimestamp(3).toLocalDateTime().toLocalDate()));
             }
@@ -92,9 +98,8 @@ public class DBOperationsUtils {
 
     public static List<BankAccount> readBankAccountsDB(Connection connection) {
         List<BankAccount> bankAccounts = new ArrayList<>();
-        String query = "SELECT * FROM bank_accounts ORDER BY status ASC, created_at ASC";
         try (Statement statement = connection.createStatement()) {
-            ResultSet set = statement.executeQuery(query);
+            ResultSet set = statement.executeQuery(GET_ACCOUNTS_QUERY);
             while (set.next()) {
                 bankAccounts.add(new BankAccount(set.getString(2), ConverterUtils.toAccountStatus(set.getString(3)),
                         EncryptorDecryptorUtils.decrypt(set.getString(4)), set.getTimestamp(5).toLocalDateTime().toLocalDate()));
@@ -107,8 +112,7 @@ public class DBOperationsUtils {
     }
 
     public static void deleteBankAccountDB(Connection connection, BankAccount account) {
-        String query = "UPDATE bank_accounts SET status = ? WHERE merchant_id = ? AND account_number = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_STATUS_QUERY)) {
             statement.setString(1, "deleted");
             statement.setString(2, account.getMerchantId());
             statement.setString(3, EncryptorDecryptorUtils.encrypt(account.getAccountNumber()));
@@ -118,8 +122,7 @@ public class DBOperationsUtils {
         }
     }
     public static void deleteMerchantBankAccountsDB(Connection connection, Merchant merchant) {
-        String query = "DELETE FROM bank_accounts WHERE merchant_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_ACCOUNTS_QUERY)) {
             statement.setString(1, merchant.getId());
             statement.execute();
         } catch (SQLException e) {
@@ -128,8 +131,7 @@ public class DBOperationsUtils {
     }
 
     public static void deleteMerchantDB(Connection connection, Merchant merchant) {
-        String query = "DELETE FROM merchants WHERE id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_MERCHANT_QUERY)) {
             statement.setString(1, merchant.getId());
             statement.execute();
         } catch (SQLException e) {
@@ -138,8 +140,7 @@ public class DBOperationsUtils {
     }
 
     public static void updateBankAccountDB(Connection connection, BankAccount bankAccount, String newBankAccountNumber) {
-        String query = "UPDATE bank_accounts SET account_number = ? WHERE merchant_id = ? AND account_number = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_ACCOUNT_NUMBER_QUERY)) {
             statement.setString(1, EncryptorDecryptorUtils.encrypt(newBankAccountNumber));
             statement.setString(2, bankAccount.getMerchantId());
             statement.setString(3, EncryptorDecryptorUtils.encrypt(bankAccount.getAccountNumber()));
